c416c32e1c43c6b12856776862b8f866acfc356f
pflanagan <pflanagan@grubhub.com>
2025-09-04 00:45:10 +0800

    layout the page

--- src/main.js
+++ src/main.js
@@ -1,2 +1,20 @@
 
+function loadCSV() {
+    $.ajax({
+        type: "GET",
+        url: "data.txt",
+        dataType: "text",
+        success: function(data) {processData(data);}
+     });
+}
 
+function focusInput() {
+  
+}
+
+(function() {
+  const displayCharacter = $('#display-characters');
+})();
+
+$(document).ready(function() {
+});
