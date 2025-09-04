b68e8d6039d0ad42f5983446de8d174c236728be
pflanagan <pflanagan@grubhub.com>
2025-09-04 09:30:02 +0800

    cursor animation

--- src/main.js
+++ src/main.js
@@ -1,20 +1,44 @@
 
+let DICTIONARY;
+
 function loadCSV() {
-    $.ajax({
-        type: "GET",
-        url: "data.txt",
-        dataType: "text",
-        success: function(data) {processData(data);}
-     });
+  $.ajax({
+    type: "GET",
+    url: "data/dictionary.txt",
+    dataType: "text",
+    success: function (data) {
+      console.log(data);
+      DICTIONARY = data;
+    }
+  });
 }
 
 function focusInput() {
-  
+
 }
 
-(function() {
+function onChange() {
+  console.log()
+}
+
+function onKeyDown() {
+
+}
+
+function getSelection() {
+  var inputField = $('#myInput')[0]; // Get the DOM element from the jQuery object
+
+  if (inputField) {
+    var start = inputField.selectionStart;
+    var end = inputField.selectionEnd;
+    var selectedText = inputField.value.substring(start, end);
+
+    console.log("Selected Text:", selectedText);
+  }
+}
+
+(function () {
+  loadCSV();
   const displayCharacter = $('#display-characters');
 })();
 
-$(document).ready(function() {
-});
