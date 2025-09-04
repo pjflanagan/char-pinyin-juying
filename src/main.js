300564fb882cc2379d7b81a6707ce25a56fa078b
pflanagan <pflanagan@grubhub.com>
2025-09-04 14:55:07 +0800

    hide settings and input

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 
+// ---------------------------------------------------------------------------
+// Constants -----------------------------------------------------------------
+// ---------------------------------------------------------------------------
+
 const IS_MANDARIN_REGEX = /[\p{Script=Han}]/u;
 
 const TONE_MAP = [
@@ -9,6 +13,106 @@ const TONE_MAP = [
   'ˋ',  // 4 -> falling
   '˙'   // 5 -> neutral
 ];
+
+// ---------------------------------------------------------------------------
+// Dictionary ----------------------------------------------------------------
+// ---------------------------------------------------------------------------
+
+let DICTIONARY;
+
+function loadCSV() {
+  fetch(`${window.location.href}/data/dictionary.csv`)
+    .then(response => response.text())
+    .then(v => Papa.parse(v, {
+      header: true,
+      complete: (result) => {
+        DICTIONARY = result.data;
+      }
+    }))
+    .catch(err => console.error(err))
+}
+
+function findEntry(hanzi) {
+  return DICTIONARY.find(entry => entry.hanziTraditional === hanzi || entry.hanziSimplified === hanzi);
+}
+
+function getTone(tone) {
+  return TONE_MAP[typeof tone === 'number' ? tone : parseInt(tone)];
+}
+
+// ---------------------------------------------------------------------------
+// Event handlers ------------------------------------------------------------
+// ---------------------------------------------------------------------------
+
+function focusInputEnd() {
+  const input = $('#hidden-input');
+  input.focus();
+  input[0].selectionStart = input[0].selectionEnd = input.val().length;
+  renderCursorAtEnd();
+}
+
+// focuses the input at a specific character
+function focusInputAt(e) {
+  const input = $('#hidden-input')[0];
+  input.focus();
+  const characterElement = $(e.target).parents('.character')[0]
+  const index = characterElement.getAttribute('data-index');
+  const side = e.target.getAttribute('data-hitbox');
+  const delta = side === 'left' ? 0 : 1;
+  const cursorPosition = index + delta;
+  input.setSelectionRange(cursorPosition, cursorPosition);
+  renderCursorAtCharacterElement(characterElement, side);
+  e.stopPropagation();
+}
+
+function handleInput(e) {
+  renderText(e.target.value);
+}
+
+function handleKeyUp(e) {
+  renderCursorAtInputCursorPosition()
+}
+
+// Gets the position of the cursor
+// TODO:
+function getSelection() {
+  const input = $('#hidden-input')[0]; // Get the DOM element from the jQuery object
+
+  if (input) {
+    const start = input.selectionStart;
+    const end = input.selectionEnd;
+  }
+}
+
+// ---------------------------------------------------------------------------
+// Render methods ------------------------------------------------------------
+// ---------------------------------------------------------------------------
+
+// Cursor
+
+function renderCursorAtEnd() {
+  $('#display-characters').append($("#cursor"));
+}
+
+function renderCursorAtCharacterElement(elem, side) {
+  if (side === 'left') {
+    $("#cursor").insertBefore(elem);
+  } else {
+    $("#cursor").insertAfter(elem);
+  }
+}
+
+function renderCursorAtInputCursorPosition() {
+  const input = $('#hidden-input')[0];
+  const cursorIndex = input.selectionStart;
+  const characterElements = $('.character');
+  if (cursorIndex >= characterElements.length) {
+    renderCursorAtEnd();
+  }
+  renderCursorAtCharacterElement(characterElements[cursorIndex], 'left')
+}
+
+// Characters
 
 function renderNonHanziCharacter(index, char) {
   return `
@@ -41,100 +145,6 @@ function renderHanziCharacter({ index, hanzi, pinyin, zhuyin, tone }) {
 `
 }
 
-// dictionary
-
-let DICTIONARY;
-
-function loadCSV() {
-  fetch(`${window.location.href}/data/dictionary.csv`)
-    .then(response => response.text())
-    .then(v => Papa.parse(v, {
-      header: true,
-      complete: (result) => {
-        DICTIONARY = result.data;
-        console.log(DICTIONARY);
-      }
-    }))
-    .catch(err => console.log(err))
-}
-
-function findEntry(hanzi) {
-  return DICTIONARY.find(entry => entry.hanziTraditional === hanzi || entry.hanziSimplified === hanzi);
-}
-
-function getTone(tone) {
-  return TONE_MAP[typeof tone === 'number' ? tone : parseInt(tone)];
-}
-
-// event handlers
-
-function focusInputEnd() {
-  console.log('focusInputEnd');
-  const input = $('#hidden-input');
-  input.focus();
-  input[0].selectionStart = input[0].selectionEnd = input.val().length;
-  renderCursorAtEnd();
-}
-
-// focuses the input at a specific character
-function focusInputAt(e) {
-  console.log('focusInputAt');
-  const input = $('#hidden-input')[0];
-  input.focus();
-  const characterElement = $(e.target).parents('.character')[0]
-  const index = characterElement.getAttribute('data-index');
-  const side = e.target.getAttribute('data-hitbox');
-  const delta = side === 'left' ? 0 : 1;
-  const cursorPosition = index + delta;
-  input.setSelectionRange(cursorPosition, cursorPosition);
-  renderCursorAtCharacterElement(characterElement, side);
-  e.stopPropagation();
-}
-
-function handleInput(e) {
-  renderText(e.target.value);
-}
-
-function handleKeyUp(e) {
-  renderCursorAtInputCursorPosition()
-}
-
-// Gets the position of the cursor
-// TODO:
-function getSelection() {
-  const input = $('#hidden-input')[0]; // Get the DOM element from the jQuery object
-
-  if (input) {
-    const start = input.selectionStart;
-    const end = input.selectionEnd;
-  }
-}
-
-// Render methods
-
-function renderCursorAtEnd() {
-  $('#display-characters').append($("#cursor"));
-}
-
-function renderCursorAtCharacterElement(elem, side) {
-  if (side === 'left') {
-    $("#cursor").insertBefore(elem);
-  } else {
-    $("#cursor").insertAfter(elem);
-  }
-}
-
-function renderCursorAtInputCursorPosition() {
-  const input = $('#hidden-input')[0];
-  const cursorIndex = input.selectionStart;
-  console.log(cursorIndex);
-  const characterElements = $('.character');
-  if (cursorIndex >= characterElements.length) {
-    renderCursorAtEnd();
-  }
-  renderCursorAtCharacterElement(characterElements[cursorIndex], 'left')
-}
-
 function renderText(text) {
   $('#display-characters').children('.character').remove();
   for (let i = 0; i < text.length; ++i) {
@@ -160,7 +170,9 @@ function renderText(text) {
   renderCursorAtInputCursorPosition();
 }
 
-// onload
+// ---------------------------------------------------------------------------
+// Onload --------------------------------------------------------------------
+// ---------------------------------------------------------------------------
 
 (function () {
   loadCSV();
