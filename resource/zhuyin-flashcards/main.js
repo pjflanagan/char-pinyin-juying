49f6837260deca3eacbda20332f3d50ba6425a03
pflanagan <pflanagan@grubhub.com>
2025-09-15 13:23:15 -0400

    add zhuyin flashcards

---
+++ resource/zhuyin-flashcards/main.js
@@ -0,0 +1,66 @@
+
+// global and state
+
+let DICTIONARY = [];
+
+let isHidden = true;
+let currentIndex = 0;
+
+// helpers
+
+
+function next() {
+  currentIndex = Math.floor(Math.random() * DICTIONARY.length);
+  display(DICTIONARY[currentIndex].trad);
+  hide();
+}
+
+// listeners
+
+function revealOrNext() {
+  if (isHidden) {
+    reveal();
+  } else {
+    next();
+  }
+}
+
+// DISPLAY
+
+function hide() {
+  $('.character .hanzi').each(function() {
+    $(this).addClass('hidden');
+  });
+  $('.pinyin').each(function() {
+    $(this).addClass('hidden');
+  });
+  $('#reveal-or-next-button').text('Reveal');
+  isHidden = true;
+}
+
+function reveal() {
+  $('.character .hanzi').each(function() {
+    $(this).removeClass('hidden');
+  });
+  $('.pinyin').each(function() {
+    $(this).removeClass('hidden');
+  });
+  $('#reveal-or-next-button').text('Next');
+  isHidden = false;
+}
+
+function display(text) {
+  $('#display-characters').children('.character').remove();
+  for (let i = 0; i < text.length; ++i) {
+    const html = getCharacterHtml(DICTIONARY, text[i], i);
+    $('#display-characters').append(html);
+  }
+}
+
+// MAIN
+
+(async function () {
+  const dictionary = await loadCsv('data/dictionary');
+  DICTIONARY = dictionary.filter(entry => entry.trad && entry.zhuyin && entry.pinyin);
+  next();
+})();
